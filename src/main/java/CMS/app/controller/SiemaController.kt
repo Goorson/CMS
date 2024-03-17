9ab6789b19e42f7466b44ff11412906b428dae95
Goorson <10407543@ucn.dk>
2024-03-17 14:38:59 +0100

    more fixes

---
+++ src/main/java/CMS/app/controller/SiemaController.kt
@@ -0,0 +1,20 @@
+package CMS.app.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
+
+
+@Controller
+@RequestMapping("/")
+class SiemaController {
+    @RequestMapping()
+    fun greeting(@RequestParam(name = "name", required = false, defaultValue = "World") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "greeting"
+    }
+
+}
