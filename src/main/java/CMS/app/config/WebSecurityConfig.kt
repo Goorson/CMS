8fb451bbb0dbdf4b715a27b61ae1ec3ff1871000
Goorson <10407543@ucn.dk>
2024-04-07 11:14:45 +0200

    send help

--- src/main/java/CMS/app/config/WebSecurityConfig.kt
+++ src/main/java/CMS/app/config/WebSecurityConfig.kt
@@ -1,6 +1,7 @@
 package CMS.app.config
 
 import CMS.app.service.impl.UserDetailsServiceImpl
+import jakarta.annotation.Resource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -8,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+
+
+
 
 
 @Configuration
@@ -44,4 +50,11 @@ class WebSecurityConfig (
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
+//
+//    @Bean
+//    fun addResourceHandlers(registry: ResourceHandlerRegistry): ResourceHandlerRegistration {
+//        return registry
+//            .addResourceHandler("/resources/**")
+//            .addResourceLocations("/resources/")
+//    }
 }
