3f71181726fed4821f3e801044f18b9ba5108e88
Goorson <10407543@ucn.dk>
2024-04-20 21:37:37 +0200

    1 fix

--- src/main/java/CMS/app/entity/User.kt
+++ src/main/java/CMS/app/entity/User.kt
@@ -7,15 +7,11 @@ import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "user")
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = -1,
-
-    val username: String = "",
-
-    val password: String = "",
-
-    val role: String? = null
+    val id: Int,
+    val username: String,
+    val password: String,
+    val role: String?
 )
--- src/main/java/CMS/app/repository/UserRepository.kt
+++ src/main/java/CMS/app/repository/UserRepository.kt
@@ -3,6 +3,6 @@ package CMS.app.repository
 import CMS.app.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Int> {
     fun findByUsername(username: String): User?
 }
